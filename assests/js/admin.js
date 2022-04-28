67d01d4e346e203a0ee4b71be24e42399b3a88de
Samuel Rofail <srofail.16@gmail.com>
2022-04-28 16:58:38 +0200

    28/4

--- assests/js/admin.js
+++ assests/js/admin.js
@@ -11,3 +11,71 @@ function adminmenu(){
 
     }
 }
+
+let genre = document.querySelectorAll(".genreEdit");
+
+
+
+function edit(){
+    var inputs = document.querySelectorAll(".editing");
+
+    var btns = document.querySelectorAll(".editor");
+    for(var i =0 ; i< btns.length ; i++){
+        btns[i].style.display = "block";
+    }
+    for(var x = 0 ; x< inputs.length; x++){
+        inputs[x].removeAttribute("disabled");
+        inputs[x].style.border = "1px solid #000";
+    }
+    for (let z =0 ; z< genre.length; z++){
+        genre[z].style.cursor ="pointer";
+    }
+}
+
+function addkind(){
+    var newkind = window.prompt();
+    var span = document.createElement("span");
+    span.setAttribute("class", "badge bg-secondary text-light genreEdit");
+    span.innerText = newkind;
+    var genre = document.getElementById("movie-kind");
+    genre.insertAdjacentElement("afterbegin", span);
+}
+function addgenre(){
+    var newkind = window.prompt();
+    var span = document.createElement("span");
+    span.setAttribute("class", "badge bg-secondary text-light genreEdit");
+    span.innerText = newkind;
+    var genre = document.getElementById("movie-genre");
+    genre.insertAdjacentElement("afterbegin", span);
+
+    
+
+
+}
+
+function addrole(){
+    var cast = document.getElementById("roles");
+    console.log(cast);
+    var e = document.createElement("div");
+    e.setAttribute("class", "col-6")
+    e.innerHTML = "<input type='text'  value='الدور:' class='editing'><input type='text'  value='actor name' class='editing'>";
+    cast.insertAdjacentElement("beforeend", e);
+
+
+}
+for (let i =0 ; i< genre.length; i++){
+    
+    genre[i].addEventListener("click" , function(){
+        if(genre[i].style.cursor == "pointer"){
+            if(confirm("You will Remove a Genre") == true)
+            genre[i].remove();
+        }
+        else{
+            console.log(genre[i]);
+
+        }
+        
+        
+
+    });
+}
